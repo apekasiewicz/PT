96deb61015bc5894104f0e2dc42ff54cf126bdd0
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-11-01 23:50:08 +0100

    Added directory for task1, with implementation of Book and Reader classes.

---
+++ Task1/LibraryProject/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+	class Book
+	{
+		private int id;
+		private string title;
+		private string author;
+		private int publishmentYear;
+
+		public Book(int bookId, string bookTitle, string bookAuthor, int bookYear)
+		{
+			id = bookId;
+			title = bookTitle;
+			author = bookAuthor;
+			publishmentYear = bookYear;
+		}
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+
+		}
+
+		public string Title
+		{
+			get { return title; }
+			set { title = value; }
+
+		}
+
+		public string Author
+		{
+			get { return author; }
+			set { author = value; }
+
+		}
+
+		public int PublishmentYear
+		{
+			get { return publishmentYear; }
+			set { publishmentYear = value; }
+		}
+
+	}
+}
---
+++ Task1/LibraryProject/Reader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+	class Reader
+	{
+		private string firstName;
+		private string lastName;
+		private int id;
+
+		public Reader(string userFirstName, string userLastName, int userId)
+		{
+			firstName = userFirstName;
+			lastName = userLastName;
+			id = userId;
+
+		}
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+	}
+}
