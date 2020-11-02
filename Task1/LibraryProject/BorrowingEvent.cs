522243f1fabb41d0567a23c98b1d2c1d15f703d8
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-11-02 01:18:36 +0100

    Added State, Event and DataContext classes with initial implementation.

---
+++ Task1/LibraryProject/BorrowingEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+	class BorrowingEvent
+	{
+		private Reader reader;
+		private State state;
+		private DateTime borrow_date;
+
+		public BorrowingEvent(Reader eReader, State eState, DateTime date)
+		{
+			reader = eReader;
+			state = eState;
+			borrow_date = date;
+		}
+
+		public DateTime Borrow_date
+		{
+			get { return borrow_date; }
+			set { borrow_date = value; }
+		}
+	}
+}
---
+++ Task1/LibraryProject/State.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+	class State
+	{
+		private Book book;
+		private DateTime borrowDate;
+		private DateTime dueDate;
+
+		public State(Book sBook, DateTime bDate, DateTime dDate)
+		{
+			book = sBook;
+			borrowDate = bDate;
+			dueDate = dDate;
+		}
+
+		public DateTime BorrowDate
+		{
+			get { return borrowDate; }
+			set { borrowDate = value; }
+		}
+
+		public DateTime DueDate
+		{
+			get { return dueDate; }
+			set { dueDate = value; }
+		}
+	}
+}
