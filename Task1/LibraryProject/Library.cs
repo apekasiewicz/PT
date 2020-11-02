a7f5791cdbfc533b3d6d77d5776fb76f85abbd87
Karolina Maciejewska <2249096@edu.p.lodz.pl>
2020-11-02 11:27:15 +0100

    Renamed DataContext class to Library.cs

---
+++ Task1/LibraryProject/Library.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace LibraryProject
+{
+    class Library
+    {
+        public List<Reader> readers = new List<Reader>();
+        public Dictionary<int, Book> books = new Dictionary<int, Book>();
+        public List<State> states = new List<State>();
+        public List<BorrowingEvent> events = new List<BorrowingEvent>();
+    }
+}
