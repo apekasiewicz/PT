a6cf5a52323704de35a156bf87513a485d2614f9
Karolina Maciejewska <2249096@edu.p.lodz.pl>
2020-11-13 23:37:54 +0100

    Added repository interface.

---
+++ Task1/LibraryProject/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+
+    //CRUD definition (create, remove, update, delete)
+{
+    interface IRepository
+    {
+        //Products
+        List<Book> GetAllBooks();
+        Book GetBookById(int id);
+        Book GetBookByGenre(BookGenre genre);
+        void UpdateBookInfo(Book book);
+        void AddBook(Book book);
+        void DeleteBook(int id);
+
+        //Readers
+        List<Reader> GetAllReaders();
+        Reader GetReaderById(int id);
+        void UpdateReaderInfo(Reader reader);
+        void AddReader(Reader reader);
+        void DeleteReader(int id);
+
+        //Events
+        List<BorrowingEvent> GetAllBorrowingEvents();
+        BorrowingEvent GetBorrowingEventById(int id);
+        void UpdateEventInfo(BorrowingEvent e);
+        void AddBorrowingEvent(BorrowingEvent e);
+        void DeleteBorrowingEvent(int id);
+
+        //States?
+    }
+}
