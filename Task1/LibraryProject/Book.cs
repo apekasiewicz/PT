9dec703d8831888e43ce2e4a23d973f785cf88c9
Karolina Maciejewska <2249096@edu.p.lodz.pl>
2020-11-13 22:59:07 +0100

    Added BookGenre enum to define the type of book.

--- Task1/LibraryProject/Book.cs
+++ Task1/LibraryProject/Book.cs
@@ -14,13 +14,14 @@ namespace LibraryProject
 			PublishmentYear = bookYear;
 		}
 
-        public int Id { get; set; }
+		public int Id { get; set; }
 
-        public string Title { get; set; }
+		public string Title { get; set; }
 
-        public string Author { get; set; }
+		public string Author { get; set; }
 
-        public int PublishmentYear { get; set; }
+		public int PublishmentYear { get; set; }
 
+		public BookGenre Genre { get; set; } 
     }
 }
---
+++ Task1/LibraryProject/BookGenre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    internal enum BookGenre
+    {
+        History,
+        Romance, 
+        Drama,
+        SciFi,
+        Criminal,
+        Kids, 
+        Adventure
+    }
+}
