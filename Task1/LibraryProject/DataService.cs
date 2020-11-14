67446780154b6dfd6d3e95d5c1d8b75099c82c7f
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-11-14 16:25:23 +0100

    Added services for getting, searching, adding, removing and editing Book class objects and Reader class objects.

--- Task1/LibraryProject/DataService.cs
+++ Task1/LibraryProject/DataService.cs
@@ -13,6 +13,7 @@ namespace LibraryProject
             this.repository = repository;
         }
 
+        // GET ALL 
         public Dictionary<int, Book> GetAllBooks()
         {
             Dictionary<int, Book> books = repository.GetAllBooks();
@@ -26,10 +27,88 @@ namespace LibraryProject
             }
         }
 
+        public List<Reader> getAllReaders()
+        {
+            List<Reader> readers = repository.GetAllReaders();
+            if (readers.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return readers;
+            }
+        }
+
+        public List<Event> getAllEvents()
+        {
+            List<Event> events = repository.GetAllEvents();
+            if (events.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return events;
+            }
+        }
+
+
+        // SEARCH FOR ONE
         public Book GetBookById(int id)
         {
             return repository.GetBookById(id);
         }
 
+        public Book GetBookByGenre(BookGenre genre)
+        {
+            return repository.GetBookByGenre(genre);
+        }
+
+        public Reader GetReaderById(int id)
+        {
+            return repository.GetReaderById(id);
+        }
+
+        public Event GetEventById(int id)
+        {
+            return repository.GetEventById(id);
+        }
+
+
+        //ADD 
+        public void AddBook(Book book)
+        {
+            repository.AddBook(book);
+        }
+
+        public void AddReader(Reader reader)
+        {
+            repository.AddReader(reader);
+        }
+
+
+        //EDIT
+        public void EditBook(Book book)
+        {
+            repository.UpdateBookInfo(book);
+        }
+
+        public void EditReader(Reader reader)
+        {
+            repository.UpdateReaderInfo(reader);
+        }
+
+
+        //REMOVE
+        public void DeleteBook(int id)
+        {
+            repository.DeleteBook(id);
+        }
+
+        public void DeleteReader(int id)
+        {
+            repository.DeleteReader(id);
+        }
     }
 }
