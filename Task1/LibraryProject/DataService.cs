5d4d8876b347b0c78c5cc8ed562cfa3ed924ca24
Karolina Maciejewska <2249096@edu.p.lodz.pl>
2020-11-14 14:57:52 +0100

    Added initial DataService class.

---
+++ Task1/LibraryProject/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    class DataService
+    {
+        private IRepository repository;
+
+        public DataService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            List<Book> books = repository.GetAllBooks();
+            if(books.Count == 0)
+            {
+                return null;
+            } 
+            else
+            {
+                return books;
+            }
+        }
+
+        public Book GetBookById(int id)
+        {
+            return repository.GetBookById(id);
+        }
+
+    }
+}
