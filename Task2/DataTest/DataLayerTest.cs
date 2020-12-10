9700454051e50164d736c2adeda077d1e16fead8
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-12-10 14:11:12 +0100

    Added tests

--- Task2/DataTest/DataLayerTest.cs
+++ Task2/DataTest/DataLayerTest.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Library.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace DataTest
 {
     [TestClass]
-    public class UnitTest1
+    public class DataLayerTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        static private string GetConnectionString()
         {
+            // To avoid storing the connection string in your code,
+            // you can retrieve it from a configuration file.
+            return "Data Source=DELL-LAT5490-2;Initial Catalog=library;Integrated Security=True";
+        }
+
+        private static void OpenSqlConnection()
+        {
+
+        }
+
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void AddBookToDatabase()
+        {
+            LibraryDataContext db = new LibraryDataContext();
+
+            Book book1 = new Book();
+            book1.book_id = 6;
+            book1.title = "Lord of the Rings";
+            book1.author = "J.R.R.Tolkien";
+            book1.publishment_year = 1954;
+            book1.genre = "Adeventure";
+
+            db.Books.InsertOnSubmit(book1);
+            db.SubmitChanges();
+
+            /*Book book2 = db.Books.SingleOrDefault(b => b.book_id.Equals(6));
+            Assert.AreEqual(book2.title, "Lord of the Rings");
+            Assert.AreEqual(book2.author, "J.R.R.Tolkien");
+            Assert.AreEqual(book2.publishment_year, 1954);
+            Assert.AreEqual(book2.genre, "Adeventure");*/
+
+            db.Books.DeleteOnSubmit(book1);
+            db.SubmitChanges();
         }
     }
 }
