c5ee2a9bf75ef60671909c791826f07dc8dde1cd
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-12-10 11:40:54 +0100

    Added initial implementation of BookService

--- Task2/Library.Services/BookService.cs
+++ Task2/Library.Services/BookService.cs
@@ -6,19 +6,132 @@ namespace Library.Services
 {
     public class BookService
     {
-        private LibraryDataContext context = new LibraryDataContext();
-
-        public List<Book> GetAllBooks()
+        public IEnumerable<Book> GetAllBooks()
         {
-            return new List<Book>(context.Books);
+            using (var context = new LibraryDataContext())  //inside using it gets closed automatically
+            {
+                var result = context.Books.ToList();
+                return result;
+            }
         }
 
-        /*Dictionary<int, Book> GetAllBooks();
-        int GetAllBooksNumber();
-        Book GetBookById(int id);
-        Book GetBookByGenre(BookGenre genre);
-        void UpdateBookInfo(Book book);
-        void AddBook(Book book);
-        void DeleteBook(int id);*/
+        public int GetAllBooksNumber()
+        {
+            using (var context = new LibraryDataContext())
+            {
+                return context.Books.Count();
+            }
+        }
+
+        public Book GetBook(string title, string author)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                foreach (Book b in context.Books.ToList())
+                {
+                    if (b.title.Equals(title) && b.author.Equals(author))
+                    {
+                        return b;
+                    }
+                }
+                return null; 
+            }
+        }
+
+        public IEnumerable<Book> GetBooksByGenre(string g)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                List<Book> result = new List<Book>();
+                foreach (Book b in context.Books)
+                {
+                    if (b.genre.Equals(g))
+                    {
+                        result.Add(b);
+                    }
+                }
+                return result;
+            }
+        } 
+
+        public bool AddBook(string a, string t, int year, string g)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                if (GetBook(t, a) == null) //if the book doesn't already exist in the db
+                {
+                    Book newBook = new Book
+                    {
+                        author = a,
+                        title = t,
+                        publishment_year = year,
+                        genre = g
+                    };
+                    context.Books.InsertOnSubmit(newBook);  //add to collection
+                    context.SubmitChanges();    //add to db
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool UpdateBookAuthor(int _id, string author) //not sure if id will work with the view
+        {
+            using (var context = new LibraryDataContext())
+            {
+                Book book = context.Books.SingleOrDefault(i => i.book_id == _id);
+                //if (book != null) always not null?
+                //{
+                    book.author = author;
+                    context.SubmitChanges();
+                    return true;
+                //}
+                //return false;
+            }
+        }
+
+        public bool UpdateBookTitle(int _id, string title)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                Book book = context.Books.SingleOrDefault(i => i.book_id == _id);
+                book.title = title;
+                context.SubmitChanges();
+                return true;
+            }
+        }
+
+        public bool UpdateBookYear(int _id, int year)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                Book book = context.Books.SingleOrDefault(i => i.book_id == _id);
+                book.publishment_year = year;
+                context.SubmitChanges();
+                return true;
+            }
+        }
+
+        public bool UpdateBookGenre(int _id, string genre)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                Book book = context.Books.SingleOrDefault(i => i.book_id == _id);
+                book.genre = genre;
+                context.SubmitChanges();
+                return true;
+            }
+        }
+
+        public bool DeleteBook(int _id)
+        {
+            using (var context = new LibraryDataContext())
+            {
+                Book book = context.Books.SingleOrDefault(i => i.book_id == _id);
+                context.Books.DeleteOnSubmit(book);
+                context.SubmitChanges();
+                return true;
+            }
+        }
     }
 }
