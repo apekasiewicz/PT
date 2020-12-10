be2a7580b6f1dee8e880ad8afe5ce943e78f77a9
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-12-10 16:08:03 +0100

    Added methods for testing DB - add, delete, and fetch elements.

--- Task2/DataTest/DataLayerTest.cs
+++ Task2/DataTest/DataLayerTest.cs
@@ -8,50 +8,61 @@ namespace DataTest
     [TestClass]
     public class DataLayerTest
     {
-        static private string GetConnectionString()
-        {
-            // To avoid storing the connection string in your code,
-            // you can retrieve it from a configuration file.
-            return "Data Source=DELL-LAT5490-2;Initial Catalog=library;Integrated Security=True";
-        }
-
-        private static void OpenSqlConnection()
-        {
-
-        }
-
-
-
-        [TestInitialize]
-        public void Initialize()
-        {
-
-        }
-
-
+        
         [TestMethod]
         public void AddBookToDatabase()
         {
             LibraryDataContext db = new LibraryDataContext();
 
             Book book1 = new Book();
-            book1.book_id = 6;
-            book1.title = "Lord of the Rings";
-            book1.author = "J.R.R.Tolkien";
-            book1.publishment_year = 1954;
-            book1.genre = "Adeventure";
+            book1.title = "Game of Thrones";
+            book1.author = "George R. R. Martin";
+            book1.publishment_year = 2011;
+            book1.genre = "Adventure";
 
             db.Books.InsertOnSubmit(book1);
             db.SubmitChanges();
 
-            /*Book book2 = db.Books.SingleOrDefault(b => b.book_id.Equals(6));
-            Assert.AreEqual(book2.title, "Lord of the Rings");
-            Assert.AreEqual(book2.author, "J.R.R.Tolkien");
-            Assert.AreEqual(book2.publishment_year, 1954);
-            Assert.AreEqual(book2.genre, "Adeventure");*/
+            Book book2 = db.Books.FirstOrDefault(b => b.title.Equals("Game of Thrones"));
+            Assert.IsNotNull(book2);
+            Assert.AreEqual(book2.title, "Game of Thrones");
+            Assert.AreEqual(book2.author, "George R. R. Martin");
+            Assert.AreEqual(book2.publishment_year, 2011);
+            Assert.AreEqual(book2.genre, "Adventure");
+        }
+        
 
-            db.Books.DeleteOnSubmit(book1);
+        [TestMethod]
+        public void DeleteBookFromDatabase()
+        {
+            LibraryDataContext db = new LibraryDataContext();
+
+            Book book = db.Books.FirstOrDefault(b => b.title.Equals("Game of Thrones"));
+            Assert.IsNotNull(book);
+            db.Books.DeleteOnSubmit(book);
             db.SubmitChanges();
+        }
+        
+
+        [TestMethod]
+        public void SelectBookFromDatabase()
+        {
+            LibraryDataContext db = new LibraryDataContext();
+
+            Book book = db.Books.FirstOrDefault(b => b.title.Equals("Harry Potter"));
+            Assert.AreEqual(book.title, "Harry Potter");
+            Assert.AreEqual(book.author, "J.K. Rowling");
+            Assert.AreEqual(book.publishment_year, 1997);
+            Assert.AreEqual(book.genre, "Fantasy");
+        }
+
+        [TestMethod]
+        public void SelectBookWhichNotExistsFromDatabase()
+        {
+            LibraryDataContext db = new LibraryDataContext();
+
+            Book book = db.Books.FirstOrDefault(b => b.title.Equals("Harry -------- Potter"));
+            Assert.IsNull(book);
         }
     }
 }
