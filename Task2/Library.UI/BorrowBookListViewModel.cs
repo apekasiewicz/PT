d2ff69a1b588fa5cfd43e4cc068a03d5b216613f
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-12-12 20:06:44 +0100

    Added ComboBox for borrow event

---
+++ Task2/Library.UI/BorrowBookListViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Library.Data;
+using Library.Services;
+using Library.UI.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.UI
+{
+    public class BorrowBookListViewModel : ModelViewBase
+    {
+
+        private ReaderService readerService = new ReaderService();
+        private BookService bookService = new BookService();
+
+       public BorrowBookListViewModel()
+       {
+            RefreshReaders();
+            RefreshBooks();
+    }
+
+        private void RefreshReaders()
+        {
+            Task.Run(() => this.Readers = readerService.GetReaders());
+        }
+
+        private void RefreshBooks()
+        {
+            Task.Run(() => this.Books = bookService.GetAllBooks());
+        }
+
+
+        private IEnumerable<Reader> readers;
+    public IEnumerable<Reader> Readers
+    {
+        get => readers;
+
+        set
+        {
+            readers = value;
+            OnPropertyChanged("Readers");
+        }
+    }
+
+        private IEnumerable<Book> books;
+        public IEnumerable<Book> Books
+        {
+            get => books;
+
+            set
+            {
+                books = value;
+                OnPropertyChanged("Books");
+            }
+        }
+
+
+        public System.Windows.Input.ICommand BorrowBookCommand
+        {
+            get;
+            private set;
+        }
+
+
+        public void BorrowBook(int readerId, int bookId)
+        {
+            
+        }
+    }
+
+}
---
+++ Task2/Library.View/BorrowBookListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Library.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library.View
+{
+	/// <summary>
+	/// Logika interakcji dla klasy BorrowBookListView.xaml
+	/// </summary>
+	public partial class BorrowBookListView : UserControl
+	{
+		BorrowBookListViewModel borrowBookListViewModel = new BorrowBookListViewModel();
+		public BorrowBookListView()
+		{
+			InitializeComponent();
+			this.DataContext = borrowBookListViewModel;
+		}
+	}
+}
