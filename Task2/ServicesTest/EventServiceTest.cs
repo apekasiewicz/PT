a9d73d49b588e27b2eee5a260ab63e217a5c4736
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-12-13 02:28:23 +0100

    Some unit tests for events - not all, missing tests for update and get events

---
+++ Task2/ServicesTest/EventServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library.Data;
+using Library.Services;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ServicesTest
+{
+	[TestClass]
+	public class EventServiceTest
+	{
+        /*
+        [TestMethod]
+        public void GetEventsFromDatabaseTest()
+        {
+            Assert.AreEqual(EventService.GetAllEventsNumber(), 5);
+        }
+
+        [TestMethod]
+        public void AddEventToDatabaseTest()
+        {
+            Assert.IsTrue(EventService.AddEvent(DateTime.Now, true, 40, 54));
+            Assert.AreEqual(EventService.GetAllEventsNumber(), 6);
+
+            //delete to restore original db
+            Assert.IsTrue(EventService.DeleteEvent(EventService.GetEvents().ElementAt(5).event_id));
+        }
+
+        [TestMethod]
+        public void AddEventForNonExistingReaderToDatabaseTest()
+        {
+            Assert.IsFalse(EventService.AddEvent(DateTime.Now, true, 40, 154));
+            Assert.AreEqual(EventService.GetAllEventsNumber(), 5);
+        }
+
+        [TestMethod]
+        public void GetEventsForReaderByIdTest()
+        {
+            IEnumerable<Event> events = EventService.GetEventsForReaderById(EventService.GetEvents().ElementAt(0).reader);
+            Assert.AreEqual(events.Count(), 1);
+            Assert.AreEqual(events.ElementAt(0).is_borrowing_event, true);
+        }
+
+        [TestMethod]
+        public void GetEventForReaderByIdTest()
+        {
+            IEnumerable<Event> events = EventService.GetEventsForReaderById(EventService.GetEvents().ElementAt(0).reader);
+            Assert.AreEqual(events.Count(), 1);
+            Assert.AreEqual(events.ElementAt(0).is_borrowing_event, true);
+        }
+
+        [TestMethod]
+        public void GetBorrowingEventsTest()
+        {
+            IEnumerable<Event> events = EventService.GetBorrowingEvents();
+            Assert.AreEqual(events.Count(), 3);
+        }
+
+        [TestMethod]
+        public void GetReturningEventsTest()
+        {
+            IEnumerable<Event> events = EventService.GetReturningEvents();
+            Assert.AreEqual(events.Count(), 2);
+        }
+
+        [TestMethod]
+        public void GetEventsByDateTest()
+        {
+            IEnumerable<Event> events = EventService.GetEventsByDate(DateTime.Now);
+            Assert.AreEqual(events.Count(), 0);
+        }
+        */
+    }
+}
