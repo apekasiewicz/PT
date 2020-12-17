726782a899890f0cbfe02a6f22c73480ff5520fc
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-12-17 20:44:50 +0100

    Made window reusable

--- Task2/Library.View/AddReaderView.xaml.cs
+++ Task2/Library.View/AddReaderView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Library.UI;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace Library.View
         public AddReaderView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)    //makes window be reusable
+        {
+            e.Cancel = true;
+            this.Hide();
         }
     }
 }
