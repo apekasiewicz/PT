4cd8c14eb7836cbec0d65b03b55159f448bd3578
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-12-17 21:48:00 +0100

    Added view for books

---
+++ Task2/Library.UI/BookListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Library.Data;
+using Library.Services;
+using Library.UI.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.UI
+{
+    public class BookListViewModel : ModelViewBase 
+    {
+        private BookService bookService;
+
+        public BookListViewModel()
+        {
+            this.bookService = new BookService();
+            RefreshBooks();
+        }
+
+        private IEnumerable<Book> books;
+        public IEnumerable<Book> Books
+        {
+            get
+            {
+                return this.books;
+            }
+            set
+            {
+                this.books = value;
+                OnPropertyChanged("Books");
+                //RefreshBooks();
+            }
+        }
+
+        private void RefreshBooks()
+        {
+            Task.Run(() => this.Books = bookService.GetAllBooks());
+        }
+    }
+}
