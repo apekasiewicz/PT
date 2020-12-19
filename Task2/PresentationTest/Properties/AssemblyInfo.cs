cab23991017365c3675d1d86c23e7b4e94d78842
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2020-12-20 00:34:46 +0100

    Added tests for ViewModel

---
+++ Task2/PresentationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PresentationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PresentationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4d62d3f-f053-4770-93f0-7efac419bb76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task2/PresentationTest/ViewModelTest.cs
@@ -0,0 +1,121 @@
+﻿using Library.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace PresentationTest
+{
+    [TestClass]
+    public class ViewModelTest
+    {
+        [TestMethod]
+        public void CreatorTestMethodListView()
+        {
+            ReaderListViewModel vm = new ReaderListViewModel();
+            Assert.IsNull(vm.Book);
+            Assert.IsNull(vm.Readers);
+            Assert.IsNull(vm.Events);
+            Assert.IsNull(vm.CurrentEvent);
+            Assert.IsNull(vm.CurrentReader);
+            Assert.IsNotNull(vm.AddReaderCommand);
+            Assert.IsNotNull(vm.EditReaderCommand);
+            Assert.IsNotNull(vm.RefreshReadersCommand);
+            Assert.IsNotNull(vm.DeleteReaderCommand);
+
+            Assert.IsTrue(vm.AddReaderCommand.CanExecute(null));
+            Assert.IsTrue(vm.EditReaderCommand.CanExecute(null));
+            Assert.IsTrue(vm.RefreshReadersCommand.CanExecute(null));
+            Assert.IsTrue(vm.DeleteReaderCommand.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CreatorTestMethodAddEditView()
+        {
+            ReaderViewModel vm = new ReaderViewModel();
+            Assert.IsNull(vm.FName);
+            Assert.IsNull(vm.LName);
+            Assert.IsNull(vm.Id);
+            Assert.IsTrue(String.IsNullOrEmpty(vm.ActionText));
+            Assert.IsNotNull(vm.MessageBoxShowDelegate);
+
+            Assert.IsNotNull(vm.AddReaderCommand);
+            Assert.IsNotNull(vm.EditReaderCommand);
+
+            Assert.IsTrue(vm.AddReaderCommand.CanExecute(null));
+            Assert.IsTrue(vm.EditReaderCommand.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void AddReaderPopUpWasShownTest()
+        {
+            ReaderViewModel vm = new ReaderViewModel();
+            int _boxShowCount = 0;
+            vm.MessageBoxShowDelegate = (messageBoxText) =>
+            {
+                _boxShowCount++;
+                Assert.AreEqual("Reader Added", messageBoxText);
+            };
+            vm.ActionText = "Reader Added";
+            Assert.IsTrue(vm.AddReaderCommand.CanExecute(null));
+            vm.AddReaderCommand.Execute(null);
+            Assert.AreEqual(1, _boxShowCount);
+        }
+
+        [TestMethod]
+        public void RefreshReaders()
+        {
+            ReaderListViewModel vm = new ReaderListViewModel();
+
+            //view loads readers after 3s
+            Thread.Sleep(3000);
+            Assert.IsTrue(vm.Readers.Count() > 0);
+        }
+
+        [TestMethod]
+        public void RefreshEventsCurrentReader()
+        {
+            ReaderListViewModel vm = new ReaderListViewModel();
+
+            Thread.Sleep(3000);
+
+            var eventRefreshEventRaised = false;
+            vm.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Events")
+                {
+                    eventRefreshEventRaised = true;
+                }
+            };
+
+            vm.CurrentReader = vm.Readers.Skip(1).First();
+            Assert.IsNotNull(vm.CurrentReader);
+
+            Thread.Sleep(3000);
+            Assert.IsTrue(eventRefreshEventRaised);
+        }
+
+        [TestMethod]
+        public void RefreshBookCurrentEvent()
+        {
+            ReaderListViewModel vm = new ReaderListViewModel();
+
+            Thread.Sleep(3000);
+
+            var bookRefreshEventRaised = false;
+            vm.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Book")
+                {
+                    bookRefreshEventRaised = true;
+                }
+            };
+
+            vm.CurrentEvent = vm.Events.Skip(1).First();
+            Assert.IsNotNull(vm.CurrentEvent);
+
+            Thread.Sleep(3000);
+            Assert.IsTrue(bookRefreshEventRaised);
+        }
+    }
+}
